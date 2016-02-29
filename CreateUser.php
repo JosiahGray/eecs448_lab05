26710cc865b264bbf6487e91f1ee1d7fbea3d099
Josiah Gray <j305g268@ku.edu>
2016-02-29 16:41:59 -0600

    Add a form to CreateUser.html Add connection check to CreateUser.php

--- CreateUser.php
+++ CreateUser.php
@@ -1,1 +1,13 @@
- 
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "jgray", "jgray1", "jgray");
+
+//check connection
+if($mysqli->connect_errno)
+{
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+
+$query = "";
+
+?>
