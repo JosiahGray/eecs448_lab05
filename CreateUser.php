939f54a2c777cc0bf732cc6b8126ab4107bf4190
Josiah Gray <j305g268@ku.edu>
2016-03-07 16:13:06 -0600

    Finish CreateUser.html and CreateUser.php Complete exercise 2

--- CreateUser.php
+++ CreateUser.php
@@ -8,6 +8,18 @@ if($mysqli->connect_errno)
   exit();
 }
 
-$query = "";
+$username = $_POST["user_id"];
 
+$query = "INSERT INTO Users (user_id) VALUES ('$username');";
+
+if($result = $mysqli->query($query))
+{
+  echo "$username was successfully added to the database!\n";
+}
+else
+{
+  echo "ERROR: $username is already in the database\n or otherwise could not be added.\n";
+}
+
+echo "<br><a href='http://people.eecs.ku.edu/~jgray/eecs_448/lab05/CreateUser.html'> Return to CreateUser page </a>";
 ?>
